0c0e3f5a113fc7dde8e2a19400f206add1c50333
PurrBiscuit <spurr@articulate.com>
2015-05-28 22:27:31 -0400

    fixed capistrano production deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,4 +2,4 @@ ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
 set :ssh_options, { keys: [File.join(ENV["HOME"], ".ssh", "app")], user: "app", forward_agent: true }
 
-server 'purrbiscuit.com', roles: %w{web app}, primary: true
+server 'www.purrbiscuit.com', roles: %w{web app}, primary: true
